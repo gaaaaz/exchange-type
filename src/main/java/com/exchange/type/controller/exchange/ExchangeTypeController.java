720fa684e60311601367fd2a28f49d90c5a84add
arturogarcia_96@hotmail.com <arturogarcia_96@hotmail.com>
2023-02-17 00:38:39 -0500

    fix: controller code to webflux reactor

--- src/main/java/com/exchange/type/controller/exchange/ExchangeTypeController.java
+++ src/main/java/com/exchange/type/controller/exchange/ExchangeTypeController.java
@@ -4,7 +4,11 @@ import com.exchange.type.model.exchange.ExchangeTypeRequest;
 import com.exchange.type.model.exchange.ExchangeTypeResponse;
 import com.exchange.type.service.ExchangeTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/exchange")
@@ -13,9 +17,13 @@ public class ExchangeTypeController {
     @Autowired
     private ExchangeTypeService exchangeTypeService;
 
-    @PostMapping("/type")
-    public ExchangeTypeResponse exchangeCurrency(@RequestBody ExchangeTypeRequest request) {
-        return exchangeTypeService.exchangeOriginToDestinationCurrency(request);
+    @PostMapping(value = "/type", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<ResponseEntity<ExchangeTypeResponse>> exchangeCurrency(@RequestBody ExchangeTypeRequest request) {
+        return exchangeTypeService.exchangeOriginToDestinationCurrency(request)
+                .map(response -> ResponseEntity.ok(response))
+                .defaultIfEmpty(ResponseEntity.badRequest().build())
+                .onErrorReturn(ResponseEntity.internalServerError().build());
     }
 
 }
